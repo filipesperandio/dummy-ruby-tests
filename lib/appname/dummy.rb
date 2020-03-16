8aa07c20b5453490c0f06be8db2d240d8b54d578
Filipe Esperandio <filipesperandio@gmail.com>
2020-03-16 17:15:37 -0300

    Add master issues
    This reverts commit 72c2c7128589d57610de6e000d42f5d8ea8f29fa.

--- lib/appname/dummy.rb
+++ lib/appname/dummy.rb
@@ -1,10 +1,21 @@
 module Appname
   class Dummy
     def foo
+      return 1 if true
+      return
+      if a
+        puts "a"
+      end
+    end
+
+    def foo
       # implement something
     end
 
     def another_not_called_method
+      puts "it is not called by any spec"
+      puts "it is not called by any spec"
+      puts "it is not called by any spec"
       puts "it is not called by any spec"
     end
 
