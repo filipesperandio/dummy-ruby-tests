77ae1789f6fc0edd8bdd7efdf83059fc9a2211a2
Filipe Esperandio <filipesperandio@gmail.com>
2020-03-20 13:35:17 -0300

    add master issue

--- lib/appname.rb
+++ lib/appname.rb
@@ -2,6 +2,7 @@ module Appname
   VERSION = '0.0.0'
 
   def not_called_method
+    puts watch(%r{^spec/acceptance/steps/(.+)_steps\.rb$})    { |m| Dir[File.join("**/#{m[1]}.feature")][0] || 'spec/acceptance' } # a few more chars
     puts "it is not called by any spec"
   end
 end
